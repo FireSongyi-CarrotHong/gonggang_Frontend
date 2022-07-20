b9fce1b646fe5f9372ce4d84f7bc59024d79cfa0
geunhong Lim <seelime11@gmail.com>
2022-07-20 23:12:29 +0900

    Nextjs setting & globalStyle and theme setting

---
+++ styles/global-style.ts
@@ -0,0 +1,33 @@
+import { createGlobalStyle } from "styled-components";
+import { reset } from "styled-reset";
+// import { media } from "./theme";
+
+export const GlobalStyle = createGlobalStyle`
+    ${reset}
+    :focus {
+        outline: none;
+        border: none;
+    }
+    ::-webkit-scrollbar {
+        display: none;
+    }
+    html{
+        font-size: 11px;
+        -webkit-text-size-adjust: none;
+        font-family: -apple-system,BlinkMacSystemFont,helvetica,Apple SD Gothic Neo,sans-serif;       
+        font-display: fallback;
+
+        -ms-overflow-style: none;
+        scrollbar-width: none;
+    }
+    button {
+        background: none;
+        padding: 0;
+        border: none;
+        cursor: pointer;
+        &:disabled {
+            cursor: default;
+            fill: #f2f3f4;
+        }
+    }
+`;
