33c76ecd9ab4069ebd7dc301c1f0c669ce24dbfe
geunhong Lim <seelime11@gmail.com>
2022-07-23 16:00:02 +0900

    feat: 공유 layout 인터페이스 추가

--- shared/const.ts
+++ shared/const.ts
@@ -1,1 +1,10 @@
-export default function d() {}
+/* eslint-disable no-undef */
+import * as React from 'react';
+
+export interface BaseProps {
+  as?: keyof JSX.IntrinsicElements | React.ComponentType<any>;
+}
+
+export interface BaseLayoutProps extends BaseProps {
+  children?: React.ReactNode;
+}
