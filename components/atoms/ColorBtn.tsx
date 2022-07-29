9ecc6e7a9cf73263a3fea81919a691d9be98b1b2
songyi-p <songyi7091@gmail.com>
2022-07-29 14:20:03 +0900

    modify : ThemeColor => ColorBtn으로 수정 + box-shadow 추가

--- components/atoms/ColorBtn.tsx
+++ components/atoms/ColorBtn.tsx
@@ -2,11 +2,11 @@ import * as React from 'react';
 import { useState } from 'react';
 import styled, { css, DefaultTheme } from 'styled-components';
 
-export interface ThemeColorProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+export interface ColorBtnProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
 	color: keyof DefaultTheme['themeColor'];
 }
 
-interface ColorBtnStyleProps extends ThemeColorProps {
+interface ColorBtnStyleProps extends ColorBtnProps {
 	isClicked: boolean;
 }
 
@@ -16,7 +16,7 @@ const ColorBtnStyle = styled.button<ColorBtnStyleProps>`
 		height: 50px;
 		border-radius: 50%;
 		background-color: ${theme.themeColor[color]};
-		box-shadow: ${isClicked ? `inset 1.5px 1.5px 10px #424242` : ''};
+		box-shadow: ${isClicked ? `inset 1.5px 1.5px 10px #424242` : '0px 4px 4px rgba(0, 0, 0, 0.5)'};
 
 		&:hover {
 			transform: scale(1.1);
@@ -24,7 +24,7 @@ const ColorBtnStyle = styled.button<ColorBtnStyleProps>`
 	`}
 `
 
-export default function ThemeColor(props: ThemeColorProps) {
+export default function ColorBtn(props: ColorBtnProps) {
 	const [isClicked, setIsClicked] = useState(false);
 
 	const clickColorBtn = () => {
---
+++ components/moleculses/ColorBtnPalette.tsx
@@ -0,0 +1,21 @@
+import * as React from 'react';
+import styled from 'styled-components';
+import ColorBtn, { ColorBtnProps } from '../atoms/ColorBtn';
+
+const ColorBtnWrapper = styled.div`
+    width: 370px;
+	display: flex;
+	flex-wrap: wrap;
+	gap : 30px;
+`
+
+export default function ColorBtnPalette() {
+	const colorPalette: ColorBtnProps['color'][] = ['yellow', 'orange', 'pink', 'hotPink', 'lightRed', 'lightGreen', 'green', 'sky', 'blue', 'purple'];
+
+	return (
+		<ColorBtnWrapper>
+			{colorPalette.map(color => <ColorBtn key={color} color={color} />)}
+		</ColorBtnWrapper>
+	)
+
+}
